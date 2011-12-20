b47ead41fa1e909573e3b44e896f6d5dcb44d027
Roger Castillo <rcastillo@roger-a6200.(none)>
2011-12-20 11:19:07 -0600

    Added helper observables to exports and made crawl breadth-first

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @fileoverview Top level module includes for sitequery.
  * @author roger.castillo@loku.com (Roger Castillo)
  */
-
+exports.createObservablejQuery = require('./rxjquery.js').createObservablejQuery;
+exports.createObservableSAX = require('./lib/rxsax').createObservableSAX;
 exports.SiteCrawl = require('./lib/sitecrawl').SiteCrawl;
 exports.SiteQuery = require('./lib/sitequery').SiteQuery;
