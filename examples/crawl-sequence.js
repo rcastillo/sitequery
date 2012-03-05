efde3b1b3b257dc4fde669a3f7fa76f6a757b724
Roger Castillo <rcastillo@Rogers-MacBook-Pro.local>
2012-03-04 21:52:29 -0600

    added crawl sequence implementation and example

---
+++ examples/crawl-sequence.js
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview Example reactive SiteCrawl
+ * @author roger.castillo@loku.com (Roger Castillo)
+ */
+
+var CrawlSequence = require('../lib/crawlsequence').CrawlSequence,
+    URL = require('url');
+
+
+// create a new SiteCrawl of depth 2 with a delay of 1s between next page
+// Note: Webcrawling is delayed and will not be executed
+// until Subscription
+var crawSeq = new CrawlSequence([URL.parse('http://google.com'),
+                                 URL.parse('http://loku.com'),
+                                 URL.parse('http://bing.com')], 1000);
+
+// ask for the observable sequence and subscribe for the CrawlResult(s)
+crawSeq.Subscribe(function(crawlResult) {
+  console.log(crawlResult.crawlLink.url.href);
+},
+// on err
+function(exn){
+  console.log('Ooo dem Dukes...with exception:' + exn);
+},
+// on crawl complete
+function(){
+  console.log('SiteCrawl complete');
+});
