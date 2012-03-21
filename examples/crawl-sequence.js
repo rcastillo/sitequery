f3659efba6ec7121f22e6e4a99df5e15151516f9
Roger Castillo <rcastillo@Rogers-MacBook-Pro.local>
2012-03-21 15:52:39 -0500

    added crawl sequence with politness delay to maximizes parallelization

--- examples/crawl-sequence.js
+++ examples/crawl-sequence.js
@@ -4,43 +4,77 @@
  */
 
 var CrawlSequence = require('../lib/crawlsequence').CrawlSequence,
-    URL = require('url');
+    createCrawlSequence = require('../lib/crawlsequence').createCrawlSequence,
+    URL = require('url'),
+    Rx = require('rx').Rx;
 
 
-// create a new SiteCrawl of depth 2 with a delay of 1s between next page
-// Note: Webcrawling is delayed and will not be executed
-// until Subscription
-var crawSeq = new CrawlSequence([URL.parse('http://google.com'),
-                                 URL.parse('http://loku.com'),
-                                 URL.parse('http://bing.com')], 1000);
 
-// ask for the observable sequence and subscribe for the CrawlResult(s)
-crawSeq.Subscribe(function(crawlResult) {
-  console.log(crawlResult.crawlLink.url.href);
-},
-// on err
-function(exn){
-  console.log('Ooo dem Dukes...with exception:' + exn);
-},
-// on crawl complete
-function(){
-  console.log('CrawlSeqs complete');
+// Removed example due to deprecation
+//// create a new SiteCrawl of depth 2 with a delay of 1s between next page
+//// Note: Webcrawling is delayed and will not be executed
+//// until Subscription
+//var crawSeq = new CrawlSequence([URL.parse('http://google.com'),
+//                                 URL.parse('http://loku.com'),
+//                                 URL.parse('http://bing.com')], 1000);
+//
+//// ask for the observable sequence and subscribe for the CrawlResult(s)
+//crawSeq.Subscribe(function(crawlResult) {
+//  console.log(crawlResult.crawlLink.url.href);
+//},
+//// on err
+//function(exn){
+//  console.log('Ooo dem Dukes...with exception:' + exn);
+//},
+//// on crawl complete
+//function(){
+//  console.log('CrawlSeqs complete');
+//});
+//
+//
+//var crawSeqStrings = new CrawlSequence(['http://google.com',
+//                                 'http://loku.com',
+//                                 'http://bing.com'], 1000);
+//
+//// ask for the observable sequence and subscribe for the CrawlResult(s)
+//crawSeqStrings.Subscribe(function(crawlResult) {
+//  console.log(crawlResult.crawlLink.url.href);
+//},
+//// on err
+//function(exn){
+//  console.log('Ooo dem Dukes...with exception:' + exn);
+//},
+//// on crawl complete
+//function(){
+//  console.log('CrawlSeq Strings complete');
+//});
+
+
+var hrefArray = ['http://google.com',
+                 'http://loku.com',
+                 'http://bing.com',
+                 'http://loku.com',
+                 'http://bing.com',
+                 'http://loku.com',
+                 'http://bing.com',
+                 'http://loku.com',
+                 'http://bing.com',
+                 'http://loku.com'];
+
+
+// convert the array of hrefs to URL objecs
+var urlStream = Rx.Observable.FromArray(hrefArray).Select(function(r){
+  return URL.parse(r);
 });
 
+// politness of 1s with crawl lasting no longer than 60s
+var cs = createCrawlSequence(urlStream, 1000, 60000);
 
-var crawSeqStrings = new CrawlSequence(['http://google.com',
-                                 'http://loku.com',
-                                 'http://bing.com'], 1000);
-
-// ask for the observable sequence and subscribe for the CrawlResult(s)
-crawSeqStrings.Subscribe(function(crawlResult) {
-  console.log(crawlResult.crawlLink.url.href);
+cs.Subscribe(function(r){
+  console.log('createCrawlSequence result', r.crawlLink.url.href);
 },
-// on err
-function(exn){
-  console.log('Ooo dem Dukes...with exception:' + exn);
-},
-// on crawl complete
+function(err){},
 function(){
-  console.log('CrawlSeq Strings complete');
+  console.log('crawl completed');
 });
+
